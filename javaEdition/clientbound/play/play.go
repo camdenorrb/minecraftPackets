1564d9493c9c5b3391e2d6feb504992e01e47f68
camdenorrb <me@12oclock.dev>
2023-03-13 01:58:10 -0500

    Progress

--- javaEdition/clientbound/play/play.go
+++ javaEdition/clientbound/play/play.go
@@ -281,4 +281,256 @@ type ChunkDataAndUpdateLight struct {
 	EmptySkyLightMask   BitSet // TODO: BitSet
 	EmptyBlockLightMask BitSet // TODO: BitSet
 	SkyLightArrayCount  types.VarInt
+	// TODO: Complete
+}
+
+type WorldEvent struct {
+	Event                 int32
+	Location              types.BlockPosition
+	Data                  int32 // TODO: Enum
+	DisableRelativeVolume bool
+}
+
+type Particle struct {
+	ParticleID     types.VarInt
+	IsLongDistance bool
+	X              float64
+	Y              float64
+	Z              float64
+	OffsetX        float32
+	OffsetY        float32
+	OffsetZ        float32
+	MaxSpeed       float32
+	ParticleCount  int32
+	Data           interface{} // TODO: Complete data types
+}
+
+type UpdateLight struct {
+	ChunkX              types.VarInt
+	ChunkZ              types.VarInt
+	TrustEdges          bool
+	SkyLightMask        BitSet // TODO: BitSet
+	BlockLightMask      BitSet // TODO: BitSet
+	EmptySkyLightMask   BitSet // TODO: BitSet
+	EmptyBlockLightMask BitSet // TODO: BitSet
+	SkyLightArrayCount  types.VarInt
+	// TODO: Complete
+}
+
+type Login struct {
+	EntityID            int32
+	IsHardcore          bool
+	GameMode            uint8 // TODO: Enum GameMode
+	PreviousGameMode    uint8 // TODO: Enum GameMode
+	DimensionCount      types.VarInt
+	DimensionNames      []types.Identifier
+	RegistryCodec       NBT // TODO: NBT
+	DimensionType       types.Identifier
+	DimensionName       types.Identifier
+	HashedSeed          int64
+	MaxPlayers          types.VarInt
+	ViewDistance        types.VarInt
+	SimulationDistance  types.VarInt
+	ReducedDebugInfo    bool
+	EnableRespawnScreen bool
+	IsDebug             bool
+	IsFlat              bool
+	HasDeathLocation    bool
+	DeathDimension      *types.Identifier
+	DeathLocation       *types.BlockPosition
+}
+
+type Icon struct {
+	Type           types.VarInt // TODO: Enum
+	X              byte
+	Z              byte
+	Direction      byte
+	HasDisplayName bool
+	DisplayName    *types.Chat
+}
+
+type MapData struct {
+	MapID     types.VarInt
+	Scale     uint8
+	IsLocked  bool
+	HasIcons  bool
+	ItemCount *types.VarInt
+	Icons     []Icon
+	Columns   uint8
+	Rows      *uint8
+	X         *byte
+	Z         *byte
+	Length    *types.VarInt
+	Data      []byte
+}
+
+type Trade struct {
+	InputItem1      Slot
+	OutputItem      Slot
+	InputItem2      Slot
+	IsDisabled      bool
+	NumOfTradeUses  int32
+	MaxTradeUses    int32
+	Experience      int32
+	SpecialPrice    int32
+	PriceMultiplier float32
+	Demand          int32
+}
+
+type MerchantOffers struct {
+	WindowID          types.VarInt
+	Size              types.VarInt
+	Trades            []Trade
+	VillagerLevel     types.VarInt
+	Experience        types.VarInt
+	IsRegularVillager bool
+	CanRestock        bool
+}
+
+type UpdateEntityPosition struct {
+	EntityID types.VarInt
+	DeltaX   int16
+	DeltaY   int16
+	DeltaZ   int16
+	OnGround bool
+}
+
+type UpdateEntityPositionAndRotation struct {
+	EntityID types.VarInt
+	DeltaX   int16
+	DeltaY   int16
+	DeltaZ   int16
+	Yaw      types.Angle
+	Pitch    types.Angle
+	OnGround bool
+}
+
+type UpdateEntityRotation struct {
+	EntityID types.VarInt
+	Yaw      types.Angle
+	Pitch    types.Angle
+	OnGround bool
+}
+
+type MoveVehicle struct {
+	X     float64
+	Y     float64
+	Z     float64
+	Yaw   float32
+	Pitch float32
+}
+
+type OpenBook struct {
+	Hand types.VarInt // TODO: Enum
+}
+
+type OpenWindow struct {
+	WindowID    types.VarInt
+	WindowType  types.VarInt // TODO: Enum
+	WindowTitle types.Chat
+}
+
+type OpenSignEditor struct {
+	Location types.BlockPosition
+}
+
+type Ping struct {
+	ID int32
+}
+
+type PlaceGhostRecipe struct {
+	WindowID byte
+	RecipeID types.Identifier
+}
+
+type PlayerAbilities struct {
+	Flags               byte // TODO: Enum
+	FlyingSpeed         float32
+	FieldOfViewModifier float32
+}
+
+type PreviousMessage struct {
+	MessageID types.VarInt
+	Signature []byte
+}
+
+type PlayerChatMessage struct {
+	Sender                     uuid.UUID
+	Index                      types.VarInt
+	MessageSignaturePresent    bool
+	MessageSignature           []byte
+	Message                    string
+	Timestamp                  int64
+	Salt                       int64
+	TotalPreviousMessages      types.VarInt
+	PreviousMessages           []PreviousMessage
+	UnsignedContentPresent     bool
+	UnsignedContent            *types.Chat
+	FilterType                 types.VarInt // TODO: Enum
+	FilterTypeBits             *BitSet      // TODO: BitSet
+	ChatType                   types.VarInt // TODO: Enum
+	NetworkName                types.Chat
+	IsNetworkTargetNamePresent bool
+	NetworkTargetName          *types.Chat
+}
+
+// EndCombat "Unused by the Notchian client. This data was once used for twitch.tv metadata circa 1.8.f"
+type EndCombat struct {
+	Duration types.VarInt
+	EntityID int32
+}
+
+// EnterCombat "Unused by the Notchian client. This data was once used for twitch.tv metadata circa 1.8.f"
+type EnterCombat struct{}
+
+type CombatDeath struct {
+	PlayerID types.VarInt
+	EntityID int32
+	Message  types.Chat
+}
+
+type PlayerInfoRemove struct {
+	NumOfPlayers types.VarInt
+	Players      []uuid.UUID
+}
+
+type PlayerInfoUpdate struct {
+	// TODO: Complete
+}
+
+type LookAt struct {
+	FeetOrEyes       types.VarInt // TODO: Enum
+	TargetX          float64
+	TargetY          float64
+	TargetZ          float64
+	IsEntity         bool
+	EntityID         *types.VarInt
+	EntityFeetOrEyes *types.VarInt // TODO: Enum
+}
+
+type SynchronizePlayerPosition struct {
+	X               float64
+	Y               float64
+	Z               float64
+	Yaw             float32
+	Pitch           float32
+	Flags           byte // TODO: Enum
+	TeleportID      types.VarInt
+	DismountVehicle bool
+}
+
+type UpdateRecipeBook struct {
+	Action                       types.VarInt // TODO: Enum
+	CraftingBookOpen             bool
+	CraftingBookFilterActive     bool
+	SmeltingBookOpen             bool
+	SmeltingBookFilterActive     bool
+	BlastFurnaceBookOpen         bool
+	BlastFurnaceBookFilterActive bool
+	SmokerBookOpen               bool
+	SmokerBookFilterActive       bool
+	RecipeIDSize1                types.VarInt
+	RecipeIDs1                   []types.Identifier
+	RecipeIDSize2                types.VarInt
+	RecipeIDs2                   []types.Identifier
 }
