53aae557540dd9dfbc202c863617e7126499580c
camdenorrb <me@12oclock.dev>
2023-03-14 21:06:12 -0500

    Things

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/camdenorrb/minecraftPackets
-
-go 1.20
-
-require github.com/joomcode/errorx v1.1.0
--- javaEdition/clientbound/play/play.go
+++ javaEdition/clientbound/play/play.go
@@ -2,6 +2,7 @@ package play
 
 import (
 	"github.com/camdenorrb/minecraftPackets/javaEdition/common"
+	"github.com/camdenorrb/minecraftPackets/nbt"
 	"github.com/google/uuid"
 )
 
