0a7bb2affde3326fd3e827c8e37a0b5d416a7f00
Camden <me@12oclock.dev>
2023-09-15 02:12:12 -0500

    Fix response

--- javaEdition/clientbound/status/status.go
+++ javaEdition/clientbound/status/status.go
@@ -1,25 +1,33 @@
 package status
 
-type Response struct {
-	Version struct {
-		Name     string `json:"name"`
-		Protocol int    `json:"protocol"`
-	}
-	Players struct {
-		Max    uint `json:"max"`
-		Online uint `json:"online"`
-		Sample []struct {
-			Name string `json:"name"`
-			ID   string `json:"id"`
-		}
-	}
-	Description struct {
-		Text string `json:"text"`
-	}
-	Favicon           string `json:"favicon"`
-	EnforceSecureChat bool   `json:"enforceSecureChat"`
-}
-
 type PingResponse struct {
 	Payload int64
+}
+
+type Response struct {
+	Version           Version     `json:"version"`
+	Players           Players     `json:"players"`
+	Description       Description `json:"description"`
+	Favicon           string      `json:"favicon"`
+	EnforceSecureChat bool        `json:"enforceSecureChat"`
+}
+
+type Version struct {
+	Name     string `json:"name"`
+	Protocol int    `json:"protocol"`
+}
+
+type Players struct {
+	Max    uint `json:"max"`
+	Online uint `json:"online"`
+	Sample []PlayerSample
+}
+
+type PlayerSample struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
+
+type Description struct {
+	Text string `json:"text"`
 }
