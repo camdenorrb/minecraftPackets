64d2bc5cacaf4190cbc6817087dca0cf93bacd3c
Camden <me@12oclock.dev>
2023-09-17 01:39:39 -0500

    Update bitset

--- primitive/bitset.go
+++ primitive/bitset.go
@@ -3,43 +3,43 @@ package primitive
 import "fmt"
 
 type BitSet struct {
-	data []uint64 // Can't inline type, since we need to change this value in the methods
+	Data []uint64 // Can't inline type, since we need to change this value in the methods
 }
 
 func NewBitSet(size uint) BitSet {
 	return BitSet{
-		data: make([]uint64, (size+63)/64),
+		Data: make([]uint64, (size+63)/64),
 	}
 }
 
 func (b *BitSet) Get(index uint) bool {
-	return b.data[index/64]&(1<<(index%64)) != 0
+	return b.Data[index/64]&(1<<(index%64)) != 0
 }
 
 func (b *BitSet) Set(index uint) {
 	b.expandFor(index)
-	b.data[index/64] |= 1 << (index % 64)
+	b.Data[index/64] |= 1 << (index % 64)
 }
 
 func (b *BitSet) Flip(index uint) {
 	b.expandFor(index)
-	b.data[index/64] ^= 1 << (index % 64)
+	b.Data[index/64] ^= 1 << (index % 64)
 }
 
 func (b *BitSet) Length() uint {
-	return uint(len(b.data) * 64)
+	return uint(len(b.Data) * 64)
 }
 
 func (b *BitSet) expandFor(index uint) {
-	if index >= uint(len(b.data)*64) {
-		difference := (int(index)-len(b.data))/64 + 1
-		b.data = append(b.data, make([]uint64, difference, difference)...)
+	if index >= uint(len(b.Data)*64) {
+		difference := (int(index)-len(b.Data))/64 + 1
+		b.Data = append(b.Data, make([]uint64, difference)...)
 	}
 }
 
 func (b *BitSet) String() string {
 	var str string
-	for _, v := range b.data {
+	for _, v := range b.Data {
 		str += fmt.Sprintf("%064b", v)
 	}
 	return str
