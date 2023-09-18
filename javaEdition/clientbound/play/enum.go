39db0bf9ff27e629488beedf494f93fb6662a38b
Camden <me@12oclock.dev>
2023-09-18 00:04:57 -0500

    Fix player info update

--- javaEdition/clientbound/play/enum.go
+++ javaEdition/clientbound/play/enum.go
@@ -3,10 +3,10 @@ package play
 type PlayerInfoMask = uint8
 
 const (
-	PlayerInfoAddPlayer         PlayerInfoMask = 0x01
-	PlayerInfoInitializeChannel PlayerInfoMask = 0x02
-	PlayerInfoUpdateGameMode    PlayerInfoMask = 0x04
-	PlayerInfoUpdateListed      PlayerInfoMask = 0x08
-	PlayerInfoUpdateLatency     PlayerInfoMask = 0x10
-	PlayerInfoUpdateDisplayName PlayerInfoMask = 0x20
+	PlayerInfoMaskAddPlayer         PlayerInfoMask = 0x01
+	PlayerInfoMaskInitializeChannel PlayerInfoMask = 0x02
+	PlayerInfoMaskUpdateGameMode    PlayerInfoMask = 0x04
+	PlayerInfoMaskUpdateListed      PlayerInfoMask = 0x08
+	PlayerInfoMaskUpdateLatency     PlayerInfoMask = 0x10
+	PlayerInfoMaskUpdateDisplayName PlayerInfoMask = 0x20
 )
