f07dc0ccf6fd3a573aeb09d3c5e317ae0cc7df81
Camden <me@12oclock.dev>
2023-09-23 01:04:07 -0500

    Fix Palette

--- javaEdition/common/chunk.go
+++ javaEdition/common/chunk.go
@@ -3,20 +3,17 @@ package common
 import "github.com/camdenorrb/minecraftPackets/primitive"
 
 type SkyLightArray struct {
-	Length primitive.VarInt
-	Data   []byte
+	Data []byte
 }
 
 type BlockLightArray struct {
-	Length primitive.VarInt
-	Data   []byte
+	Data []byte
 }
 
 type PalettedContainerStructure struct {
-	BitsPerEntry    uint8
-	Palette         Palette
-	DataArrayLength primitive.VarInt
-	DataArray       []int64
+	BitsPerEntry uint8
+	Palette      Palette
+	DataArray    []int64
 }
 
 type Palette struct {
@@ -30,8 +27,7 @@ type SingleValuedPalette struct {
 }
 
 type IndirectPalette struct {
-	PaletteLength primitive.VarInt
-	Palette       []primitive.VarInt
+	Palette []primitive.VarInt
 }
 
 type DirectPalette struct{}
